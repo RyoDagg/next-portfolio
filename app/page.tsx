a50406c9cfb6899555f6d1d1a6517579630156ca
ryodagg <mannai.abdlkader@gmail.com>
2025-02-04 13:20:10 +0100

    Add FloatingNav component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import Hero from '@/components/hero';
+import { FloatingNav } from '@/components/ui/floating-navbar';
+
+const navItems = [
+  {
+    name: 'Home',
+    link: '#',
+  },
+  {
+    name: 'About',
+    link: '#',
+  },
+  {
+    name: 'Contact',
+    link: '#',
+  },
+];
 
 export default function Home() {
   return (
     <main className="bg-black-100 flex flex-col items-center justify-center mx-auto overflow-hidden p-5 relative sm:px-10">
       <div className="max-w-6xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
     </main>
