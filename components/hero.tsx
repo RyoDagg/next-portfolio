48703f9519c61eb3a5c3217a116703476f379067
ryodagg <mannai.abdlkader@gmail.com>
2025-02-05 14:28:22 +0100

    hero tweaks

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,17 +2,22 @@ import { HeroCard } from './ui/hero-card';
 import { DotBackground } from './ui/dot-background';
 import { Spotlight } from './ui/spotlight';
 import { TypewriterEffect } from './ui/typewriter-effect';
+import { PiTerminalWindowDuotone } from 'react-icons/pi';
 
 const Hero = () => {
   return (
     <DotBackground>
-      <div className="h-screen w-full max-w-6xl relative p-8">
+      <div className="h-screen w-full max-w-7xl relative p-8">
         <Spotlight />
         <div className="grid grid-cols-1 md:grid-cols-3 mt-28">
-          <div className="flex flex-col items-start justify-start col-span-2 h-full w-full text-center">
-            <h2 className="text-3xl uppercase font-semibold bg-clip-text text-transparent bg-gradient-to-b from-blue-50 to-gray-400">
+          <div className="flex flex-col items-start gap-4 justify-start col-span-2 h-full w-full text-center">
+            <h2 className="text-4xl uppercase font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue-50 to-gray-400">
               Abdelkader Mannai
             </h2>
+            <h3 className="flex items-center gap-2 text-3xl uppercase font-semibold text-gray-200">
+              <PiTerminalWindowDuotone className="w-12 h-12 text-teal-500" />{' '}
+              Full-Stack Developer
+            </h3>
             <TypewriterEffect
               className="my-4 uppercase font-semibold text-left text-7xl"
               words={[
@@ -48,7 +53,7 @@ const Hero = () => {
                 },
               ]}
             />
-            <p className="text-xl font-medium text-gray-200 mt-8">
+            <p className="text-2xl font-medium text-gray-200 mt-8">
               <span className="underline italic font-bold">#KIS</span>: Keep It
               Simple 😉
             </p>
