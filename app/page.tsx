d58b2241041038a6fa99ba760aceecda6bd6ed83
ryodagg <mannai.abdlkader@gmail.com>
2025-02-09 18:00:55 +0100

    under construction

--- app/page.tsx
+++ app/page.tsx
@@ -24,16 +24,17 @@ export default function Home() {
   return (
     <main className="bg-black-100 flex flex-col items-center justify-center mx-auto relative">
       <div className="w-full">
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <About />
         <Technologies />
         <Projects />
-        <div className="h-[128rem]">
+        <Grid />
+        <div className="h-[32rem]">
           <div className="flex justify-center items-center mt-10 gap-4">
-            <IoConstruct className="text-7xl text-gray-200" />
-            <h1 className="text-7xl text-gray-200 text-center">
-              COMING SOON...
+            <IoConstruct className="text-6xl text-gray-200" />
+            <h1 className="text-5xl font-semibold uppercase text-gray-200 text-center">
+              under construction...
             </h1>
           </div>
         </div>
