1dd718ff096c38985ef2670d1af30f16340756ec
ryodagg <mannai.abdlkader@gmail.com>
2025-02-09 09:03:47 +0100

    add overflow-hidden to DotBackground component

--- components/ui/dot-background.tsx
+++ components/ui/dot-background.tsx
@@ -1,6 +1,6 @@
 export function DotBackground({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="w-full bg-dot-gray-700 relative flex items-center justify-center">
+    <div className="w-full bg-dot-gray-700 relative flex items-center justify-center overflow-hidden">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {children}
